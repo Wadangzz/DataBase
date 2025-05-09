d8fa0d6633b9c87efab2411eb6f26b475bfcd110
Wadangzz <151944861+Wadangzz@users.noreply.github.com>
2025-05-09 11:11:06 +0900

    init
    DB server에 mes로 로그인해서
    DataBase 조회 가능

---
+++ DB/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbTableSelect = new ComboBox();
+            tb_User = new TextBox();
+            tb_Password = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnConnect = new Button();
+            btnDisconnect = new Button();
+            label3 = new Label();
+            btnLoadTable = new Button();
+            dataGridView1 = new DataGridView();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            statusStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // cmbTableSelect
+            // 
+            cmbTableSelect.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbTableSelect.FormattingEnabled = true;
+            cmbTableSelect.Items.AddRange(new object[] { "OK", "NG" });
+            cmbTableSelect.Location = new Point(191, 131);
+            cmbTableSelect.Name = "cmbTableSelect";
+            cmbTableSelect.Size = new Size(134, 23);
+            cmbTableSelect.TabIndex = 0;
+            // 
+            // tb_User
+            // 
+            tb_User.Location = new Point(191, 36);
+            tb_User.Name = "tb_User";
+            tb_User.Size = new Size(134, 23);
+            tb_User.TabIndex = 1;
+            // 
+            // tb_Password
+            // 
+            tb_Password.Location = new Point(191, 65);
+            tb_Password.Name = "tb_Password";
+            tb_Password.PasswordChar = '*';
+            tb_Password.Size = new Size(134, 23);
+            tb_Password.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(111, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 3;
+            label1.Text = "username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(111, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 4;
+            label2.Text = "password";
+            // 
+            // btnConnect
+            // 
+            btnConnect.Location = new Point(381, 39);
+            btnConnect.Name = "btnConnect";
+            btnConnect.Size = new Size(153, 23);
+            btnConnect.TabIndex = 5;
+            btnConnect.Text = "DB Connect";
+            btnConnect.UseVisualStyleBackColor = true;
+            btnConnect.Click += button1_Click;
+            // 
+            // btnDisconnect
+            // 
+            btnDisconnect.Location = new Point(381, 68);
+            btnDisconnect.Name = "btnDisconnect";
+            btnDisconnect.Size = new Size(153, 23);
+            btnDisconnect.TabIndex = 6;
+            btnDisconnect.Text = "DB Disconnect";
+            btnDisconnect.UseVisualStyleBackColor = true;
+            btnDisconnect.Click += btnDisconnect_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(111, 134);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Select_Table";
+            // 
+            // btnLoadTable
+            // 
+            btnLoadTable.Location = new Point(381, 130);
+            btnLoadTable.Name = "btnLoadTable";
+            btnLoadTable.Size = new Size(153, 24);
+            btnLoadTable.TabIndex = 8;
+            btnLoadTable.Text = "DB Check";
+            btnLoadTable.UseVisualStyleBackColor = true;
+            btnLoadTable.Click += btnLoadTable_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(22, 175);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(623, 291);
+            dataGridView1.TabIndex = 9;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(0, 483);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(675, 22);
+            statusStrip1.TabIndex = 10;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(121, 17);
+            toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(675, 505);
+            Controls.Add(statusStrip1);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnLoadTable);
+            Controls.Add(label3);
+            Controls.Add(btnDisconnect);
+            Controls.Add(btnConnect);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(tb_Password);
+            Controls.Add(tb_User);
+            Controls.Add(cmbTableSelect);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbTableSelect;
+        private TextBox tb_User;
+        private TextBox tb_Password;
+        private Label label1;
+        private Label label2;
+        private Button btnConnect;
+        private Button btnDisconnect;
+        private Label label3;
+        private Button btnLoadTable;
+        private DataGridView dataGridView1;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
---
+++ DB/Form1.cs
@@ -0,0 +1,140 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace DB
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection conn;
+        private bool isConnected = false;
+
+        private CancellationTokenSource? tokenSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //cmbTableSelect.Items.AddRange(new string[] { "OK", "NG" });
+            //cmbTableSelect.SelectedIndex = 0;
+
+            btnLoadTable.Enabled = false;
+            btnDisconnect.Enabled = false;
+        }
+
+
+        private void CurrentTime()
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = tb_User.Text.Trim();
+            string password = tb_Password.Text;
+
+            string[] allowedUsers = { "mes" };
+            string[] restrictChar = { ";", "--", "'", "#", "=", "%", "/*", "*/", "\"", "\\" };
+
+            if (!allowedUsers.Contains(user))
+            {
+                MessageBox.Show("허용되지 않은 사용자 형식입니다.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (string c in restrictChar)
+            {
+                if (user.Contains(c) || password.Contains(c))
+                {
+                    MessageBox.Show("비밀번호에 허용되지 않은 문자가 포함되어 있습니다.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+            string strConn = @$"server=10.10.24.179;
+                            port=3306;database=product_db;
+                            uid={user};password={password};";
+            try
+            {
+                conn = new MySqlConnection(strConn);
+                conn.Open();
+                isConnected = conn.State == ConnectionState.Open;
+
+                if (isConnected)
+                {
+                    btnLoadTable.Enabled = true;
+                    btnDisconnect.Enabled = true;
+                    btnConnect.Enabled = false;
+                    tb_User.Text = "";
+                    tb_Password.Text = "";
+                    MessageBox.Show("DB 연결 성공");
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+        }
+
+        private void LoadTable(string tableName)
+        {
+            try
+            {
+                string query = $"SELECT * FROM {tableName} ORDER BY id ASC";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"데이터 조회 실패: {ex.Message}");
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            tokenSource = new CancellationTokenSource();
+            await StartPolling(tokenSource.Token);
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (conn != null && isConnected)
+            {
+                conn.Close();
+                isConnected = conn.State == ConnectionState.Closed;
+                MessageBox.Show("DB 연결 해제됨.");
+
+                btnLoadTable.Enabled = false;
+                btnDisconnect.Enabled = false;
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void btnLoadTable_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                try
+                {
+                    string tableName = cmbTableSelect.SelectedItem.ToString();
+                    LoadTable(tableName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"테이블 로드 실패: {ex.Message}");
+                }
+
+            }
+        }
+
+        private async Task StartPolling(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Run(() => CurrentTime());
+                await Task.Delay(1, token);
+            }
+        }
+    }
+}
